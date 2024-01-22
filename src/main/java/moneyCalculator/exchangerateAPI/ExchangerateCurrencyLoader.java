2274559a4bad66596ac422cc22d5c13659fda27f
AlejandroOrtegaG <alejandroog99@gmail.com>
2024-01-22 02:39:10 +0000

    API implemented

--- src/main/java/moneyCalculator/exchangerateAPI/ExchangerateCurrencyLoader.java
+++ src/main/java/moneyCalculator/exchangerateAPI/ExchangerateCurrencyLoader.java
@@ -1,4 +1,42 @@
 package moneyCalculator.exchangerateAPI;
 
-public class ExchangerateCurrencyLoader {
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import moneyCalculator.Currency;
+import moneyCalculator.CurrencyLoader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Collections.emptyList;
+
+public class ExchangerateCurrencyLoader implements CurrencyLoader {
+    @Override
+    public List<Currency> load() {
+        try {
+            return toList(loadJson());
+        } catch (IOException e) {
+            return emptyList();
+        }
+    }
+
+    private List<Currency> toList(String json) {
+        List<Currency> list = new ArrayList<>();
+        List<JsonElement> symbols = new Gson().fromJson(json, JsonObject.class)
+                .get("supported_codes").getAsJsonArray().asList();
+        for (JsonElement symbol : symbols)
+            list.add(new Currency(symbol.getAsJsonArray().asList().get(0).getAsString(),symbol.getAsJsonArray().asList().get(1).getAsString()));
+        return list;
+    }
+
+    private String loadJson() throws IOException {
+        URL url = new URL("https://v6.exchangerate-api.com/v6/6ea3787c99e31d55eb4378b4/codes");
+        try (InputStream is = url.openStream()) {
+            return new String(is.readAllBytes());
+        }
+    }
 }
--- src/main/java/moneyCalculator/exchangerateAPI/ExchangerateExchangeRateLoader.java
+++ src/main/java/moneyCalculator/exchangerateAPI/ExchangerateExchangeRateLoader.java
@@ -1,4 +1,35 @@
 package moneyCalculator.exchangerateAPI;
 
-public class ExchangerateExchangeRateLoader {
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import moneyCalculator.Currency;
+import moneyCalculator.ExchangeRate;
+import moneyCalculator.ExchangeRateLoader;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ExchangerateExchangeRateLoader implements ExchangeRateLoader {
+
+    @Override
+    public ExchangeRate load(Currency from, Currency to) {
+        try {
+            URL url = new URL("https://v6.exchangerate-api.com/v6/6ea3787c99e31d55eb4378b4/latest/"+from.code());
+            InputStream input = url.openStream();
+            JsonObject js = new Gson().fromJson(new String(input.readAllBytes()), JsonObject.class);
+            return new ExchangeRate(from,to,
+                    LocalDate.parse(js.get("time_last_update_utc").getAsString(), DateTimeFormatter.RFC_1123_DATE_TIME),
+                    js.get("conversion_rates").getAsJsonObject().asMap().get(to.code()).getAsDouble());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
